ac3e65e72f74083fc7740143eb4fe1a3cbbf975a
woblerr <antkurochkin@gmail.com>
2019-10-30 16:52:15 +0300

    Fix exit_code when test failed

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 image="ibmcom/mq:9.1.2.0"
+exit_code=0
 
 set -x
 
-docker pull ${image}
+docker pull $image
 docker build -t mq_test -f tests/Dockerfile .
 
 container_id=$(docker container run --env LICENSE=accept --env MQ_QMGR_NAME=QM1 --publish 1414:1414 --publish 9443:9443 --detach mq_test)
 
 sleep 60
 
-printf "Run unit tests"
+docker container exec -it $container_id /bin/bash ./tests/prep_env.sh
+docker container exec -it $container_id python2.7 -m pytest --cov=.
 
-docker container exec -it ${container_id} /bin/bash ./tests/prep_env.sh
-docker container exec -it ${container_id} python2.7 -m pytest --cov=.
+[ $? -ne 0 ] && exit_code=1
 
 container_id=$(docker container run --env LICENSE=accept --env MQ_QMGR_NAME=QM1 --publish 1415:1414 --publish 9444:9443 --detach mq_test)
 
 sleep 60
 
-docker container exec -it ${container_id} /bin/bash ./tests/prep_env.sh
-docker container exec -it ${container_id} python3.6 -m pytest --cov=.
+docker container exec -it $container_id /bin/bash ./tests/prep_env.sh
+docker container exec -it $container_id python3.6 -m pytest --cov=.
+
+[ $? -ne 0 ] && exit_code=1
+
+exit $exit_code
